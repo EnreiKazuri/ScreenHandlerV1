609d18ff7b7770ac573d68b2c381a86d1bcb3081
Muirghen <vasquezmariano01@gmail.com>
2023-01-15 17:10:20 -0400

    Changed json test file to generic path

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-ScreenHandler handler = new ScreenHandler(@"C:\Users\namit\Desktop\ScreenHandlerV1\JsonScreens\Screen1.json");
+ScreenHandler handler = new ScreenHandler(@"./JsonScreens/Screen1.json");
